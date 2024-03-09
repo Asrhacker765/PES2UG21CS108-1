8275cc7f46adb47e096fa9cc8acbf0db240a3746
AYUSH SISODIYA <92314240+Asrhacker765@users.noreply.github.com>
2024-03-09 14:53:03 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -4,8 +4,5 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout << "webhook create\n";
   return 0;
 }
